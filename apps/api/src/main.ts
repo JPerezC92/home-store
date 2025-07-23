af1f932263b043e23fe01a8e765005e5780cffea
Philip Junior Pérez Castro <jperez.c92@gmail.com>
2025-07-22 19:36:10 -0500

    added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,32 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import { patchNestjsSwagger, ZodValidationPipe } from '@anatine/zod-nestjs';
 import { AppModule } from './app.module';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  patchNestjsSwagger(); // <--- This is the hacky patch using prototypes (for now)
+  
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
